d3c32b2f4809ceab7abd1da8a8a6b3608e8ba102
deeznuts-42 <127157623+deeznuts-42@users.noreply.github.com>
2023-07-10 11:41:10 +0200

    Update bleu.sh

--- bleu.sh
+++ bleu.sh
@@ -1,2 +1,2 @@
-echo "alias norminette='sh /sgoinfre/goinfre/Perso/amugnier/PUBLIC/normicheh.sh'" >> ~/.zshrc;
+echo "alias norminette='/sgoinfre/goinfre/Perso/amugnier/PUBLIC/normicheh.sh'" >> ~/.zshrc;
 echo "Troll activated !"
